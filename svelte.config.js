aaa342989f0520cc67f041b7ad71f65dae32fb4c
Josh Oertel <16182568+joshwashywash@users.noreply.github.com>
2023-03-27 19:15:40 -0700

    why oh why do you have to do this?

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,14 @@
 import adapter from '@sveltejs/adapter-static';
 
-const dev = process.argv.includes('dev');
+// const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-		paths: {
-			base: dev ? '' : '/screensavers'
-		}
+		adapter: adapter()
+		// paths: {
+		// 	base: dev ? '' : '/screensavers'
+		// }
 	}
 };
 export default config;
